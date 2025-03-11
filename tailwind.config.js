1e92eaa9127dd9dd85631d222c367887d9fd65f6
Bijoykarmokar <bijoykarmokar798@gmail.com>
2025-03-11 19:06:39 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    content: ['./your-files/**/*.{html,js}'],
+    // other stuff...
+   daisyui: {
+     themes: ['light', 'dark', 'cupcake'],
+   },
+  plugins: [require("daisyui")],
+ }
